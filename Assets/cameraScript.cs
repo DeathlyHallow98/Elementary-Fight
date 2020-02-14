c0c4de5f4e867bb5a5762b3718a6c86010380ddc
IHATETHIS <JUSTWORK@WORK.COM>
2020-02-14 02:46:01 -0800

    first commit

---
+++ Assets/cameraScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraScript : MonoBehaviour
+{
+    public Transform target;
+    public float smoothSpeed;
+    public Vector3 offset;
+
+    private void FixedUpdate()
+    {
+        Vector3 finalPos = target.position + offset;
+        Vector3 smoothPos = Vector3.Lerp(transform.position, finalPos, smoothSpeed * Time.deltaTime);
+        transform.position = smoothPos;
+
+        transform.LookAt(target);
+    }
+}
