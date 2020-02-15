75ddb8f828bba3990da33c519c38d7a5cd9b77be
Umair Khalid <mumairkhalid30@gmail.com>
2020-02-15 02:03:04 -0800

    shooting (Unstable)

---
+++ Assets/miniAttack.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class miniAttack : MonoBehaviour
+{
+    public GameObject projPreFab;
+    public float drag = 0;
+    public float scale = 2f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("pressed");
+            scale += 0.2f;
+            Attack();
+            projPreFab.GetComponent<Rigidbody>().drag = 200;
+           // projPreFab.GetComponent<Transform>().localScale = new Vector3(scale, scale, scale);
+        }
+
+        
+
+    }
+    void Attack()
+    {
+        while (!Input.GetButtonUp("Fire1"))
+        {
+            {
+                //if (Input.GetButtonDown("Fire1"))
+                //{
+                //    Debug.Log("pressed");
+                //    scale += 0.2f;
+                //    projPreFab.GetComponent<Transform>().localScale = new Vector3(scale, scale, scale);
+                //}
+                Debug.Log("s");
+            }
+        }
+            Instantiate(projPreFab, transform.position, transform.rotation);
+
+    }
+}
--- Assets/playerMovement.cs
+++ Assets/playerMovement.cs
@@ -5,24 +5,32 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour
 {
     public float moveSpeed = 10f;
-    public float jumpForce = 10f;   
+    public float jumpForce = 10f;
     private bool canJump = false;
     public float numberOfJumps = 2;
     public float gravity = -20f;
 
-    
+
     // increase the gravity
     // Player movement
     // updates the canJump boolean
     void FixedUpdate()
     {
-        Physics.gravity = new Vector3(0f, gravity, 0f);
-        Jump();
+        
         Vector3 playerMovement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += playerMovement * moveSpeed * Time.deltaTime;
-        if(numberOfJumps <1)
+        if (numberOfJumps < 1)
         {
             canJump = false;
+        }
+    }
+
+    private void Update()
+    {
+        Physics.gravity = new Vector3(0f, gravity, 0f);
+        if (Input.GetButtonDown("Jump") && canJump)
+        {
+            Jump();
         }
     }
 
@@ -30,20 +38,17 @@ public class playerMovement : MonoBehaviour
     void Jump()
     {
         Rigidbody rb = gameObject.GetComponent<Rigidbody>();
-        if (Input.GetButtonDown("Jump") && canJump )
-        {
-            rb.AddForce(new Vector2(0f, jumpForce),ForceMode.Impulse);
-            rb.velocity = new Vector2(0, 0);
-            numberOfJumps--;
-        }
-       
+        rb.AddForce(new Vector2(0f, jumpForce), ForceMode.Impulse);
+        rb.velocity = new Vector2(0, 0);
+        numberOfJumps--;
+
     }
-   
+
     //this method checks if the player is on the ground
     private void OnTriggerStay(Collider other)
     {
         canJump = true;
-     
+
     }
 
     //set jumps back to 2 when the player hits the ground
