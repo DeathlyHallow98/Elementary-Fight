2c469a45ab5c2f17e20ccb02d7773d2b2453852e
IHATETHIS <JUSTWORK@WORK.COM>
2020-02-15 02:03:04 -0800

    shooting (Unstable)

---
+++ Assets/projectile.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectile : MonoBehaviour
+{
+    public float forward;
+    public float upward;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+        if(Input.GetButtonUp("Fire1"))
+        {
+            Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+            rb.AddForce(new Vector2(forward, upward), ForceMode.Impulse);
+            rb.drag = 0;
+        }
+        
+    }
+}
