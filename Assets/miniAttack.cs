93175d0e5b4273a976d617374fe801d2650e164d
Umair Khalid <mumairkhalid30@gmail.com>
2020-02-16 02:34:38 -0800

    Improved projectile

--- Assets/miniAttack.cs
+++ Assets/miniAttack.cs
@@ -8,36 +8,27 @@ public class miniAttack : MonoBehaviour
     public float drag = 0;
     public float scale = 2f;
 
+
     // Update is called once per frame
     void Update()
     {
+
         if (Input.GetButtonDown("Fire1"))
         {
             Debug.Log("pressed");
-            scale += 0.2f;
+          
             Attack();
             projPreFab.GetComponent<Rigidbody>().drag = 200;
-           // projPreFab.GetComponent<Transform>().localScale = new Vector3(scale, scale, scale);
+            // projPreFab.GetComponent<Transform>().localScale = new Vector3(scale, scale, scale);
+
         }
 
-        
 
     }
+
     void Attack()
     {
-        while (!Input.GetButtonUp("Fire1"))
-        {
-            {
-                //if (Input.GetButtonDown("Fire1"))
-                //{
-                //    Debug.Log("pressed");
-                //    scale += 0.2f;
-                //    projPreFab.GetComponent<Transform>().localScale = new Vector3(scale, scale, scale);
-                //}
-                Debug.Log("s");
-            }
-        }
-            Instantiate(projPreFab, transform.position, transform.rotation);
+        Instantiate(projPreFab, transform.position, transform.rotation);
 
     }
 }
--- Assets/projectile.cs
+++ Assets/projectile.cs
@@ -6,21 +6,40 @@ public class projectile : MonoBehaviour
 {
     public float forward;
     public float upward;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-
-    }
-
+    public float scale = 1f;
+    private Rigidbody rb;
+    private Transform tr;
     // Update is called once per frame
+   
+    private void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        tr = gameObject.GetComponent<Transform>();
+        tr.localScale = new Vector3(scale, scale, scale);
+        Debug.Log(scale);
+        Debug.Log(tr.localScale);
+    }
     void Update()
     {
-
-        
-        if(Input.GetButtonUp("Fire1"))
+        if(Input.GetButtonDown("Fire1"))
+            {
+           // rb.drag = 200;
+        }
+        if (Input.GetButton("Fire1"))
         {
-            Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+           if(scale <=2)
+            {
+            scale += 0.1f;
+            tr.localScale = new Vector3(scale, scale, scale);
+            }
+
+          
+
+
+        //    Debug.Log("s");
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
             rb.AddForce(new Vector2(forward, upward), ForceMode.Impulse);
             rb.drag = 0;
         }
