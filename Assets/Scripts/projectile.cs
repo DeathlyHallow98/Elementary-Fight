88e795d39dfc4c3b1fcccc37d1674ca5fbbad691
IHATETHIS <JUSTWORK@WORK.COM>
2020-03-08 17:02:05 -0700

    Added Player and Projectile rotation

--- Assets/Scripts/projectile.cs
+++ Assets/Scripts/projectile.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 public class projectile : MonoBehaviour
 {
-    public float forward;
-    public float upward;
-    public float scale = 1f;
-    private Rigidbody rb;
-    private Transform tr;
+    public float speed; //speed of the bullet
+    public float upward; //upward speed
+    public float scale = 1f; //initial scale of the bullet
+    private float forward; //direction of the bullet
+    private Rigidbody rb; // bullet's rigidbody
+    private Transform tr; // bullet's transform
     private bool released = false; //so that clicking buttons again will not affect it once released
    
+    //initialize everything
     private void Start()
     {
+        forward = transform.right.x * speed; //direction times speed (-ve or +ve)
         rb = gameObject.GetComponent<Rigidbody>();
         tr = gameObject.GetComponent<Transform>();
-        tr.localScale = new Vector3(scale, scale, scale);
+        tr.localScale = new Vector3(scale, scale, scale); 
         
     }
     void Update()
@@ -37,6 +40,7 @@ public class projectile : MonoBehaviour
               
             if(!released)
             {
+                Debug.Log(forward);
                 rb.AddForce(new Vector2(forward, upward), ForceMode.Impulse);
                 rb.drag = 0;
                 Destroy(gameObject, 1);
