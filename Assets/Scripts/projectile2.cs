40e9b11454c0a0c908525235e453fe5961c7125e
laothomas01 <53920510+laothomas01@users.noreply.github.com>
2020-03-19 00:49:21 -0700

    changes

--- Assets/Scripts/projectile2.cs
+++ Assets/Scripts/projectile2.cs
@@ -4,22 +4,23 @@ using UnityEngine;
 
 public class projectile2 : MonoBehaviour
 {
-    public float forward;
-    public float upward;
-   public float scale = 1f;
-    private Rigidbody rb;
-    private Transform tr;
-    
-  
-    // Update is called once per frame
+    public float speed; //speed of the bullet
+    public float upward; //upward speed
+    public float scale = 1f; //initial scale of the bullet
+    private float forward; //direction of the bullet
+    private Rigidbody rb; // bullet's rigidbody
+    private Transform tr; // bullet's transform
+    private bool released = false; //so that clicking buttons again will not affect it once released
 
+    //initialize everything
     private void Start()
     {
+        forward = transform.right.x * speed; //direction times speed (-ve or +ve)
         rb = gameObject.GetComponent<Rigidbody>();
         tr = gameObject.GetComponent<Transform>();
-        
-       // tr.localScale = new Vector3(scale, scale, scale);
-       // Debug.Log(scale);
+
+        // tr.localScale = new Vector3(scale, scale, scale);
+        // Debug.Log(scale);
         //Debug.Log(tr.localScale);
     }
     void Update()
@@ -45,9 +46,18 @@ public class projectile2 : MonoBehaviour
         }
         if (Input.GetButtonUp("Fire2"))
         {
-            rb.AddForce(new Vector2(forward, upward), ForceMode.Impulse);
-            
-            rb.drag = 0;   
+
+            if (!released)
+            {
+                Debug.Log(forward);
+                rb.AddForce(new Vector2(forward, upward), ForceMode.Impulse);
+                rb.drag = 0;
+                Destroy(gameObject, 1);
+
+            }
+            //once set true, it should be not be set back to false!!!!
+            released = true;
+
         }
 
     }
